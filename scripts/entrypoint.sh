aa013876216107398da7a3e3038100ba74d06cb9
beketx <beketsk@gmail.com>
2021-09-26 22:01:57 +0600

    TODO: postgres ERROR"OperationalError: unable to open database file" SOLVE: CHECK PYTHON DEPLOY VIDEO

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,5 +8,5 @@ sudo python manage.py collectstatic --noinput
 
 #sudo python manage.py runserver 0.0.0.0:8000
 
-gunicorn --bind=0.0.0.0:8000 --workers=8 bilim.wsgi
+gunicorn bilim.wsgi:application --bind 0.0.0.0:8000 --preload -w 4
 
