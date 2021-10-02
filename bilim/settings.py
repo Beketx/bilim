b4f628657576c9c5d41ed9cad26cc9f5f45b5182
beketx <Almaty17>
2021-10-02 15:04:20 +0600

    A

--- bilim/settings.py
+++ bilim/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['159.89.12.164']
 ALLOWED_HOSTS.extend(
     filter(
         None,
